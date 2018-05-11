2f6699bf7d72fb4a263c797bd7873c23b9f2cdb4
Oliver Faro <mail@o-faro.de>
2018-05-11 14:41:46 +0200

    es5 class added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,78 +1,74 @@
-var formValidation = {
-  init: function(options) {
-
+var formValidation = function( options ) {
     this.options = options;
-
     var _self = this;
-    var eleArr = Object.keys(options);
-    var elements = [];
+    var eleArr = Object.keys( this.options );
 
-    for (var i = 0; i <= eleArr.length; i++) {
-      var el = $("#" + eleArr[i]);
+    for ( var i = 0; i <= eleArr.length; i++ ) {
+      var el = $( "#" + eleArr[ i ] );
 
-      el.blur(function() {
-        var element = $(this);
-        _self.checkField(element);
-      });
+      el.blur( function() {
+        var element = $( this );
+        _self.checkField( element );
+      } );
     }
-  },
+  };
 
-  checkField: function(element) {
-    var rule = this.options[$(element).attr("id")];
+  formValidation.prototype.checkField = function( element ) {
+    var rule = this.options[ $( element ).attr( "id" ) ];
 
-    if ($(element).hasClass("alert-danger")) {
-      this.closeColorBox(element);
+    if ( $( element ).hasClass( "alert-danger" ) ) {
+      this.closeColorBox( element );
     }
 
-    this.removeError(element);
+    this.removeError( element );
 
     var truthyEl =
-      $(element).val() === null ? true : $(element).val().length <= 1;
+      $( element ).val() === null ? true : $( element ).val().length <= 1;
 
-    if (truthyEl && rule.req === "required") {
-      this.addError(element, rule.msg);
+    if ( truthyEl && rule.req === "required" ) {
+      this.addError( element, rule.msg );
     } else {
-      this.removeError(element);
+      this.removeError( element );
     }
-  },
+  };
 
-  addError: function(element, msg) {
+  formValidation.prototype.addError = function( element, msg ) {
     var alertElement =
-      '<div class="modal-header">' +
-      "<strong class='text-center  center-block alert-danger alert_extend'>There is an error in your Form</strong>" +
+      "<div class=\"modal-header\">" +
+      "<strong class='text-center  center-block alert-danger alert_extend'>" +
+      "There is an error in your Form" +
+      "</strong>" +
       "</div>" +
-      '<div class="modal-body">' +
+      "<div class=\"modal-body\">" +
       "<span>" +
       msg +
       "</span>" +
       "</div>";
 
     if (
-      $(element)
-        .parent("div")
-        .find(".error").length === 0
+      $( element )
+        .parent( "div" )
+        .find( ".error" ).length === 0
     ) {
-      $(element).addClass("alert-danger");
-      $.colorbox({
+      $( element ).addClass( "alert-danger" );
+      $.colorbox( {
         html: alertElement,
         className: "modal-content",
         top: 30,
         closeButton: true,
         returnFocus: true
-      });
+      } );
     }
-  },
+  };
 
-  removeError: function(element) {
-    $(element).removeClass("alert-danger");
-  },
+  formValidation.prototype.removeError = function( element ) {
+    $( element ).removeClass( "alert-danger" );
+  };
 
-  
-  closeColorBox: function(element) {
-    $(document).bind("cbox_closed.myInput", function(elenent) {
-      $(element).focus();
-      $(document).unbind("cbox_closed.myInput");
-    });
+  formValidation.prototype.closeColorBox = function( element ) {
+    $( document ).bind( "cbox_closed.myInput", function() {
+      $( element ).focus();
+      $( document ).unbind( "cbox_closed.myInput" );
+    } );
     $.colorbox.close();
-  }
-};
+  };
